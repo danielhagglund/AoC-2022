a87823cd2c028ca37aa057115f923916e2843c2a
danielhagglund <danihagl@gmail.com>
2022-12-03 22:31:08 +0100

    Day 3

---
+++ Day3_part1/part1.py
@@ -0,0 +1,67 @@
+priorities = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+}
+
+input_data = open("./input.txt", encoding="utf8").read().splitlines()
+
+total = 0
+
+for rucksack in input_data:
+    compartmentsize = len(rucksack)
+    compartment1=slice(0,int(compartmentsize/2))
+    compartment2=slice(int(compartmentsize/2),compartmentsize)
+    duplicates = ''.join(set(rucksack[compartment1]).intersection(rucksack[compartment2]))
+    total = total + priorities[duplicates]
+    
+print(total)
